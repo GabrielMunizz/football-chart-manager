e6f5e590fdf20cbfe7bdc930a8c5357d061b0af9
Gabriel Muniz <gabrielmfd@gmail.com>
2024-02-02 17:47:38 -0300

    Elaboração do teste de leaderboard

---
+++ app/backend/src/tests/leaderboard.test.ts
@@ -0,0 +1,26 @@
+import * as sinon from 'sinon';
+import * as chai from 'chai';
+// @ts-ignore
+import chaiHttp = require('chai-http');
+
+import { app } from '../app';
+import Example from '../database/models/ExampleModel';
+
+import { Response } from 'superagent';
+
+chai.use(chaiHttp);
+
+const { expect } = chai;
+
+describe('Testes da rota /leaderboard', () => {
+
+
+  it('Deve retornar todas as partidas ao executar o método GET em "/leaderboard/home"', async () => {
+    const res = await chai.request(app).get('/leaderboard/home');
+
+    expect(res).to.have.status(200);
+    expect(res.body).to.be.an('array');
+  });
+  
+  
+});
