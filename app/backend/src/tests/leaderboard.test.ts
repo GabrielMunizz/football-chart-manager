1de7357ed5359f792ab0612c325c1d77c6e10a48
Gabriel Muniz <gabrielmfd@gmail.com>
2024-02-03 15:50:11 -0300

    Adiciona cobertura de 90% nos testes

--- app/backend/src/tests/leaderboard.test.ts
+++ app/backend/src/tests/leaderboard.test.ts
@@ -14,9 +14,22 @@ const { expect } = chai;
 
 describe('Testes da rota /leaderboard', () => {
 
+  it('Deve retornar todas as partidas ao executar o método GET em "/leaderboard/away"', async () => {
+    const res = await chai.request(app).get('/leaderboard');
+
+    expect(res).to.have.status(200);
+    expect(res.body).to.be.an('array');
+  });
 
   it('Deve retornar todas as partidas ao executar o método GET em "/leaderboard/home"', async () => {
     const res = await chai.request(app).get('/leaderboard/home');
+
+    expect(res).to.have.status(200);
+    expect(res.body).to.be.an('array');
+  });
+
+  it('Deve retornar todas as partidas ao executar o método GET em "/leaderboard/away"', async () => {
+    const res = await chai.request(app).get('/leaderboard/away');
 
     expect(res).to.have.status(200);
     expect(res.body).to.be.an('array');
